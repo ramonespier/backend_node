a81537234fde1dbe3bf8bf5c02639c1f3a2d9fb2
ramonespier <ramon.coelhomelo@outlook.com>
2025-02-13 10:00:32 -0300

    feat: modulo path

---
+++ aula_02/modulo_path/index.js
@@ -0,0 +1,11 @@
+const { arch } = require("os");
+const path = require("path");
+
+const fullPath = path.join(__dirname, "arquivo.txt");
+console.log(fullPath);
+
+const nomeArquivo = path.basename(fullPath);
+console.log(nomeArquivo);
+
+const extensao = path.extname(fullPath);
+console.log(extensao);
