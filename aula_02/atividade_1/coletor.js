fb2dcef0037138014abeb26150482de91a8c0a55
ramonespier <ramon.coelhomelo@outlook.com>
2025-02-14 09:45:50 -0300

    fix: consertando erro de escrita [object Objetc]

--- aula_02/atividade_1/coletor.js
+++ aula_02/atividade_1/coletor.js
@@ -3,13 +3,13 @@ const fs = require('fs')
 
 const plataforma = (os.platform())
 const arquitetura =(os.arch())
-const informacao = (os.cpus())
+const informacao = JSON.stringify(os.cpus(), null, 2)
 const diretorio = (os.homedir())
 const sos = (os.type())
 const versao = (os.version())
-const interface = os.networkInterfaces()
+const interface = JSON.stringify(os.networkInterfaces(), null, 2)
 
-const conteudo = 'Plataforma: ' + plataforma + '\n' + 'Arquitetura: ' + arquitetura + '\n' + 'Informação: ' + informacao + '\n' + 'Diretório: ' + diretorio + '\n' + 'Sistema Operacional: ' + sos + '\n' + 'Versão: ' + versao + '\n' + 'Interface: ' + interface
+const conteudo = 'Plataforma: ' + plataforma + '\n' + 'Arquitetura: ' + arquitetura + '\n' + 'Informação: ' + '\n' + 'Diretório: ' + diretorio + '\n' + 'Sistema Operacional: ' + sos + '\n' + 'Versão: ' + versao + '\n' + 'Interface: ' + interface + "\n" + "CPU: " + informacao;
 
 
 fs.writeFile('info_sistema.txt', conteudo, (err) => {
