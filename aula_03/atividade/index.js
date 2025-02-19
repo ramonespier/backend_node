7b8a3d712c7f2cfd0bcffa3cf83faf48cc554d98
ramonespier <ramon.coelhomelo@outlook.com>
2025-02-19 08:00:31 -0300

    feat: tratamento de erros

--- aula_03/atividade/index.js
+++ aula_03/atividade/index.js
@@ -16,44 +16,79 @@ fs.readFile("dados.json", "utf8", (err, data) => {
     console.log(
       `Nome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
     );
-    readline.question("Deseja alterar alguma informação?", (Yn) => {
-      if (Yn == "sim" || "yes" || "y") {
-        
-      }
-    });
-    readline.question("\nO que você deseja alterar? \n", (resposta) => {
-      if (resposta == "nome") {
-        readline.question("Digite o novo nome: \n", (nome) => {
-          dados.nome = nome;
-          const jsonData = JSON.stringify(dados, null, 2);
-          fs.writeFile("dados.json", jsonData, "utf8", (err) => {
-            if (err) {
-              console.log("Erro ao escrever no arquivo: ", err);
-              return;
-            }
-            console.log(
-              `Nome alterado para ${nome}\n\nNome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
-            );
-            readline.close();
-          });
+
+    readline.question("Deseja alterar alguma informação?\n", (Yn) => {
+
+      if (Yn == "sim" || Yn == "yes" || Yn == "y") {
+
+        readline.question("\nO que você deseja alterar? \n", (resposta) => {
+          if (resposta == "nome") {
+            readline.question("Digite o novo nome: \n", (nome) => {
+              dados.nome = nome;
+
+              const jsonData = JSON.stringify(dados, null, 2);
+              fs.writeFile("dados.json", jsonData, "utf8", (err) => {
+                if (err) {
+                  console.log("Erro ao escrever no arquivo: ", err);
+                  return;
+                }
+
+                console.log(
+                  `Nome alterado para ${nome}\n\nNome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
+                );
+                readline.close();
+              });
+            });
+          }
+
+          if (resposta == "idade") {
+            readline.question("Digite o novo idade: \n", (idade) => {
+              dados.idade = idade;
+              const jsonData = JSON.stringify(dados, null, 2);
+              fs.writeFile("dados.json", jsonData, "utf8", (err) => {
+                if (err) {
+                  console.log("Erro ao escrever no arquivo: ", err);
+                  return;
+                }
+                
+                console.log(
+                  `Nome alterado para ${idade}\n\nNome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
+                );
+                readline.close();
+              });
+            });
+          }
+
+          if (resposta == "cidade") {
+            readline.question("Digite o novo cidade: \n", (cidade) => {
+              dados.cidade = cidade;
+
+              const jsonData = JSON.stringify(dados, null, 2);
+              fs.writeFile("dados.json", jsonData, "utf8", (err) => {
+                if (err) {
+                  console.log("Erro ao escrever no arquivo: ", err);
+                  return;
+                }
+                
+                console.log(
+                  `Nome alterado para ${cidade}\n\nNome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
+                );
+                readline.close();
+              });
+            });
+          }
         });
+
+      } else if (Yn == 'nao' || Yn == 'n' || Yn == 'no') {
+        console.log('Beleza! Programa encerrado.')
+        readline.close()
+
+      } else {
+        console.log('Responda apenas com sim ou não.')
+        readline.close()
       }
 
-      if (resposta == "idade") {
-        readline.question("Digite a nova idade: ", (idade) => {
-          resposta.idade = idade;
-          console.log(`Idade alterada para: ${idade}`);
-          readline.close();
-        });
-      }
 
-      if (resposta == "cidade") {
-        readline.question("Digite a nova cidade: ", (cidade) => {
-          resposta.cidade = cidade;
-          console.log(`Cidade alterada para: ${cidade}`);
-          readline.close();
-        });
-      }
     });
   } catch {
     console.log(err);
