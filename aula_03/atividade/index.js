44f063ba5f7b598a9b53403317682af0167c69b0
ramonespier <ramon.coelhomelo@outlook.com>
2025-02-19 22:35:49 -0300

    feat: novo tratamento de erro. impossivel colocar sobrenome no campo "nome"

--- aula_03/atividade/index.js
+++ aula_03/atividade/index.js
@@ -14,16 +14,25 @@ fs.readFile("dados.json", "utf8", (err, data) => {
   try {
     const dados = JSON.parse(data);
     console.log(
-      `Nome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
+      `\nNome: ${dados.nome}\nSobrenome: ${dados.sobrenome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}\n`
     );
 
-    readline.question("Deseja alterar alguma informação?\n", (Yn) => {
-
-      if (Yn == "sim" || Yn == "yes" || Yn == "y") {
-
+    readline.question("Deseja alterar alguma informação? (s/n).\n", (Yn) => {
+      if (Yn == "sim" || Yn == "yes" || Yn == "y" || Yn == "s") {
         readline.question("\nO que você deseja alterar? \n", (resposta) => {
           if (resposta == "nome") {
             readline.question("Digite o novo nome: \n", (nome) => {
+              // Tratamento de erro para colocar apenas o primeiro nome
+              let nomeNovo = nome;
+              let primeiroNome = nomeNovo.split(" ");
+
+              if (primeiroNome.length > 1) {
+                console.log("\nEscreva apenas o seu primeiro nome.");
+                readline.close();
+                return;
+              }
+              ////////////////////////////////////////////////////////////
+
               dados.nome = nome;
 
               const jsonData = JSON.stringify(dados, null, 2);
@@ -34,7 +43,25 @@ fs.readFile("dados.json", "utf8", (err, data) => {
                 }
 
                 console.log(
-                  `Nome alterado para ${nome}\n\nNome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
+                  `Nome alterado para ${nome}\n\nNome: ${dados.nome}\nSobrenome: ${dados.sobrenome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}\n`
+                );
+                readline.close();
+              });
+            });
+          }
+
+          if (resposta == "sobrenome") {
+            readline.question("Digite o novo sobrenome: \n", (sobrenome) => {
+              dados.sobrenome = sobrenome;
+              const jsonData = JSON.stringify(dados, null, 2);
+              fs.writeFile("dados.json", jsonData, "utf8", (err) => {
+                if (err) {
+                  console.log("Erro ao escrever no arquivo: ", err);
+                  return;
+                }
+
+                console.log(
+                  `Sobrenome alterado para ${sobrenome}\n\nNome: ${dados.nome}\nSobrenome: ${dados.sobrenome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}\n`
                 );
                 readline.close();
               });
@@ -50,9 +77,9 @@ fs.readFile("dados.json", "utf8", (err, data) => {
                   console.log("Erro ao escrever no arquivo: ", err);
                   return;
                 }
-                
+
                 console.log(
-                  `Nome alterado para ${idade}\n\nNome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
+                  `Nome alterado para ${idade}\n\nNome: ${dados.nome}\nSobrenome: ${dados.sobrenome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}\n`
                 );
                 readline.close();
               });
@@ -69,25 +96,23 @@ fs.readFile("dados.json", "utf8", (err, data) => {
                   console.log("Erro ao escrever no arquivo: ", err);
                   return;
                 }
-                
+
                 console.log(
-                  `Nome alterado para ${cidade}\n\nNome: ${dados.nome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}`
+                  `Nome alterado para ${cidade}\n\nNome: ${dados.nome}\nSobrenome: ${dados.sobrenome}\nIdade: ${dados.idade}\nCidade: ${dados.cidade}\n`
                 );
                 readline.close();
               });
             });
           }
+          
         });
-
-      } else if (Yn == 'nao' || Yn == 'n' || Yn == 'no') {
-        console.log('Beleza! Programa encerrado.')
-        readline.close()
-
+      } else if (Yn == "nao" || Yn == "não" || Yn == "n" || Yn == "no") {
+        console.log("Beleza! Programa encerrado.");
+        readline.close();
       } else {
-        console.log('Responda apenas com sim ou não.')
-        readline.close()
+        console.log("Responda apenas com sim ou não.");
+        readline.close();
       }
-
 
     });
   } catch {
