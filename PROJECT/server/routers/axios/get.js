572c50c213c548d2c315dfa7c5e4ae2cc8054b70
ramonespier <ramon.coelhomelo@outlook.com>
2025-03-26 08:19:44 -0300

    .

--- PROJECT/server/routers/axios/get.js
+++ PROJECT/server/routers/axios/get.js
@@ -3,8 +3,10 @@ import chalk from "chalk";
 
 const token = 'SEGREDO'
 
-axios.get('http://localhost:3000/admin', { headers: {"Authorization": `Bearer: ${token}`}})
+axios.get('http://localhost:3000/admin', { headers: {"Authorization": `Bearer ${token}`}})
     .then(response => {
+        console.log(response.data.url)
+        console.log(response.data.explanation)
         console.log('Dados recebidos: ', response.data)
     })
     .catch(error => {
